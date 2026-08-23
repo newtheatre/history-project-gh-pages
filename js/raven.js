2946110a7feea08aa4e635c317503fd3e607df30
ntbot <webmaster@newtheatre.org.uk>
2026-08-23 18:42:12 +0000

    Deploying to gh-pages from  @ 7b5ae5ed5892d853a64d56e492ddaa46f66a1ff0 🚀

--- js/raven.js
+++ js/raven.js
@@ -1,3 +1,3 @@
 Raven.config('https://3c9d529e9e7b486296e6d918c22a6153@app.getsentry.com/82953', {
-    release: "0405b72ca74fefd16e19dee95653d69c2832cd1c"
+    release: "7b5ae5ed5892d853a64d56e492ddaa46f66a1ff0"
 }).install();
